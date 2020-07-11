80228a163eed92699753d3617669affcec2e5af6
HyunKi Kim <gusrl4025@naver.com>
2020-07-11 09:31:02 +0900

    Conditional Rendering - login rendering

--- src/index.js
+++ src/index.js
@@ -2,10 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import Chapter06 from './Chapter06';
+import Greeting from './Chapter07/Greeting';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<Chapter06 />, document.getElementById('root'));
+ReactDOM.render(<Greeting isLoggedIn={true}/>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
